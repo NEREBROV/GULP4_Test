7ca60104e65e57834a57ff0707b6fd13fa7ce64a
NEREBROV <riebrovn2000@gmail.com>
2022-07-28 14:52:12 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp')
+const less = require('gulp-less')
+const rename = require('gulp-rename')
+const cleanCSS = require('gulp-clean-css')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const del = require('del')
+
+
+const paths = {
+		styles: {
+			src: 'src/styles/**/*.less',
+			dest: 'dist/css/'
+		},
+		scripts: {
+			src: 'src/scripts/**/*.js',
+			dest: 'dist/js/'
+		}
+}
+
+function clean() {
+	return del(['dist'])
+}
+
+function styles() {
+	return gulp.src(paths.styles.src)
+	.pipe(less())
+	// Минификация CSS
+	.pipe(cleanCSS())
+	// переименовываем (добавляем название) файл(а)
+	.pipe(rename({
+		basename: 'main',
+		suffix: '.min'
+	}))
+	// Запись стилей в папку dist
+	.pipe(gulp.dest(paths.styles.dest))
+}
+
+function scripts() {
+	return gulp.src(paths.scripts.src, {
+		sourcemaps: true
+	})
+	.pipe(babel())
+	.pipe(uglify())
+	.pipe(concat('main.min.js'))
+	.pipe(gulp.dest(paths.scripts.dest))
+
+}
+
+function watch() {
+	gulp.watch(paths.styles.src, styles)
+	gulp.watch(paths.scripts.src, scripts)
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+
+exports.clean = clean
+exports.styles = styles
+exports.scscripts =scripts
+exports.watch = watch
+exports.build = build
+exports.default = build

f4c9c2d9c6e0d35226ccfe840841a2061e58da7c
NEREBROV <riebrovn2000@gmail.com>
2022-07-29 12:31:44 +0700

    thiird commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,47 +5,90 @@ const cleanCSS = require('gulp-clean-css')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const imagemin = require('gulp-imagemin')
+const autoprefixer = require('gulp-autoprefixer')
+const htmlmin = require('gulp-htmlmin');
+const size = require('gulp-size');
+const newer = require('gulp-newer');
+
 const del = require('del')
 
 
 const paths = {
-		styles: {
-			src: 'src/styles/**/*.less',
-			dest: 'dist/css/'
-		},
-		scripts: {
-			src: 'src/scripts/**/*.js',
-			dest: 'dist/js/'
-		}
+	html:{
+		src: 'src/*.html',
+		dest: 'dist'
+	},
+	styles: {
+		src: 'src/styles/**/*.less',
+		dest: 'dist/css/'
+	},
+	scripts: {
+		src: 'src/scripts/**/*.js',
+		dest: 'dist/js/'
+	},
+	images: {
+		src: 'src/img/*',
+		dest: 'dist/img/'
+	}
 }
 
 function clean() {
-	return del(['dist'])
+	return del(['dist/*', '!dist/img'])
+}
+
+function html() {
+	return gulp.src(paths.html.src)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(size())
+    .pipe(gulp.dest(paths.html.dest));
 }
 
 function styles() {
 	return gulp.src(paths.styles.src)
-	.pipe(less())
-	// Минификация CSS
-	.pipe(cleanCSS())
-	// переименовываем (добавляем название) файл(а)
-	.pipe(rename({
-		basename: 'main',
-		suffix: '.min'
-	}))
-	// Запись стилей в папку dist
-	.pipe(gulp.dest(paths.styles.dest))
-}
+		.pipe(sourcemaps.init())
+		// Вызов препроцессора
+		.pipe(less())
+		.pipe(autoprefixer({
+			cascade: false 
+		}))
+		// Минификация CSS
+		.pipe(cleanCSS({
+			level: 2
+		}))
+		// переименовываем (добавляем название) файл(а)
+		.pipe(rename({
+			basename: 'main',
+			suffix: '.min'
+		}))
+		// Запись стилей в папку dist
+		.pipe(sourcemaps.write('.'))
+		.pipe(size())
+		.pipe(gulp.dest(paths.styles.dest))
+	}
 
 function scripts() {
-	return gulp.src(paths.scripts.src, {
-		sourcemaps: true
-	})
-	.pipe(babel())
-	.pipe(uglify())
-	.pipe(concat('main.min.js'))
-	.pipe(gulp.dest(paths.scripts.dest))
+	return gulp.src(paths.scripts.src)
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(uglify())
+		.pipe(concat('main.min.js'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(size())
+		.pipe(gulp.dest(paths.scripts.dest))
+}
 
+function img() {
+	 return gulp.src(paths.images.src)
+	 .pipe(newer('paths.images.dest'))	
+	 .pipe(imagemin({
+			progressive: true
+		}))
+		.pipe(size())
+	 	.pipe(gulp.dest(paths.images.dest))
 }
 
 function watch() {
@@ -53,9 +96,11 @@ function watch() {
 	gulp.watch(paths.scripts.src, scripts)
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
 
 exports.clean = clean
+exports.img = img
+exports.html = html
 exports.styles = styles
 exports.scscripts =scripts
 exports.watch = watch
